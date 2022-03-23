c2858d6704bce30f012d732c77f777b46a6260ae
subham sinha <subham.sinha41@gmail.com>
2022-03-23 20:04:57 +0530

    bhailog package added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import 'bhailang';
 import SliderOne from './components/slider';
 function App() {
   return (
     <>
     <div className="App">
       <header className="App-header">
-      
+      <h1>hello</h1>
       </header>
     </div>
     <SliderOne />
